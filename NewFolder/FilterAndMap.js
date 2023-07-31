8129e2ed3390a8c235fc0df2edd98518ee98b84e
sachin <sachinraj3665@gmail.com>
2023-07-31 14:35:14 +0530

    1st commit

---
+++ NewFolder/FilterAndMap.js
@@ -0,0 +1,6 @@
+var a=[10,20,30,40,50]
+var res=a.filter((num)=>{return num>20})
+console.log(res);
+
+var res1=a.map((num)=>{return num*2})
+console.log(res1);
---
+++ NewFolder/Promise1.js
@@ -0,0 +1,11 @@
+var x =23;
+   (function(){
+     var x=43;
+      (function random(){
+       x++;
+       console.log(x) 
+      // var x=21;
+       })
+      ()
+   })
+()
